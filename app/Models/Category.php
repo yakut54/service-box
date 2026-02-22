8dd5cb8f8fcfffd01aec58f94849c3b686439b40
yakut54 <mongodb1@yandex.ru>
2026-02-22 11:24:33 +0700

    fix: revert Category to extends Model (TenantModel breaks search_path)
    TenantService sets search_path only on the default DB connection.
    Category must use the same default connection as Product/Order/etc.

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,9 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-class Category extends TenantModel
+class Category extends Model
 {
     use HasUuids, SoftDeletes;
 
