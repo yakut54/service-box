89ad4af3eb6637769b3cd8bc28396d02bfc54563
yakut54 <mongodb1@yandex.ru>
2026-02-22 18:29:16 +0700

    fix new css 7

--- widget/src/components/Cart.vue
+++ widget/src/components/Cart.vue
@@ -111,7 +111,7 @@ watch(() => cartStore.total, (newVal, oldVal) => {
             <div class="sb-cart-item-main">
               <!-- Картинка -->
               <div class="sb-cart-item-img-wrap">
-                <img v-if="item.image_url" :src="item.image_url" :alt="item.name" class="sb-cart-item-img" />
+                <div v-if="item.image_url" class="sb-cart-item-img" :style="{ backgroundImage: `url(${item.image_url})` }"></div>
                 <div v-else class="sb-cart-item-img-placeholder">
                   <svg width="20" height="20" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
@@ -152,7 +152,7 @@ watch(() => cartStore.total, (newVal, oldVal) => {
           <!-- ── Цифровой товар: 1 строка, удалить справа ── -->
           <template v-else>
             <div class="sb-cart-item-img-wrap sb-cart-item-img-wrap--digital">
-              <img v-if="item.image_url" :src="item.image_url" :alt="item.name" class="sb-cart-item-img sb-cart-item-img--contain" />
+              <div v-if="item.image_url" class="sb-cart-item-img sb-cart-item-img--contain" :style="{ backgroundImage: `url(${item.image_url})` }"></div>
               <div v-else class="sb-cart-item-img-placeholder">
                 <svg width="20" height="20" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M15 10l4.553-2.276A1 1 0 0121 8.723v6.554a1 1 0 01-1.447.894L15 14M3 8a2 2 0 012-2h8a2 2 0 012 2v8a2 2 0 01-2 2H5a2 2 0 01-2-2V8z" />
